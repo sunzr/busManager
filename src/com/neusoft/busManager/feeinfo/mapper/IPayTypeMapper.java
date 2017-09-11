fdf1c921d7030efa6125d114ffe6e24fa924cbde
admin <admin@admin-PC>
2017-09-11 16:03:02 +0800

    小修缴费类型

--- src/com/neusoft/busManager/feeinfo/mapper/IPayTypeMapper.java
+++ src/com/neusoft/busManager/feeinfo/mapper/IPayTypeMapper.java
@@ -7,28 +7,28 @@ import org.apache.ibatis.session.RowBounds;
 import com.neusoft.busManager.feeinfo.model.PayTypeModel;
 
 public interface IPayTypeMapper {
-	// 增加操作员
+	// 增加缴费类型
 	public void insert(PayTypeModel ptm) throws Exception;
 
-	// 修改操作员
+	// 修改缴费类型
 	public void update(PayTypeModel ptm) throws Exception;
 
-	// 修改操作员密码
+	// 修改缴费类型密码
 	public void updateForPasswowrd(PayTypeModel ptm) throws Exception;
 
-	// 删除操作员
+	// 删除缴费类型
 	public void delete(PayTypeModel ptm) throws Exception;
 
-	// 取得指定的操作员对象
+	// 取得指定的缴费类型对象
 	public PayTypeModel select(int PayTypeno) throws Exception;
 
-	// 取得所有操作员列表，无分页
+	// 取得所有缴费类型列表，无分页
 	public List<PayTypeModel> selectListByAll() throws Exception;
 
-	// 取得所有操作员列表，有分页
+	// 取得所有缴费类型列表，有分页
 	public List<PayTypeModel> selectListByAllWithPage(RowBounds rb) throws Exception;
 
-	// 取得操作员个数
+	// 取得缴费类型个数
 	public int selectCountByAll() throws Exception;
 
 	// 校验缴费类型名称是否存在
