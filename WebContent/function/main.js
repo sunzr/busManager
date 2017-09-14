2047d2ab6c2531e2cbfbea8f90340bae6371a8db
RoadSides <1813568679@qq.com>
2017-09-14 21:18:37 +0800

    function的一些bus修改

--- WebContent/function/main.js
+++ WebContent/function/main.js
@@ -19,7 +19,7 @@ $(function(){
 		viewrecords: true, // show the current page, data rang and total records on the toolbar
 		autowidth:true,
 		height: 370,
-		rowNum: 18,
+		rowNum: 25,
 		rowList:[10,15,20],
 		jsonReader:{
 			root:"list",
